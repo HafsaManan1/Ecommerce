f2af36f2524937416dabca4e9d8450682f106ad3
Ubuntu <ubuntu@ip-172-31-8-183.ap-south-1.compute.internal>
2025-01-19 10:22:23 +0000

    Added website status check script

--- devops/scripts/validate_service.sh
+++ devops/scripts/validate_service.sh
@@ -2,7 +2,6 @@
 
 # Exit immediately if a command exits with a non-zero status
 set -e
-health_check="https://foodmart.osolglobal.tech/"
 
 # Check Gunicorn status
 echo "Checking Gunicorn service status..."
@@ -13,16 +12,4 @@ else
     exit 1
 fi
 
-# Validate website status
-echo "Validating website status..."
-
-http_status=$(curl -o /dev/null -s -w "%{http_code}" "$health_check")
-
-if [ "$http_status" -eq 200 ]; then
-    echo "The API is healthy. Returned HTTP status: $http_status"
-    exit 0
-else
-    echo "The API is not healthy. Returned HTTP status: $http_status"
-    exit 1
-fi
 
